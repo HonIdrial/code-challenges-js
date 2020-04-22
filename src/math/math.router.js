ca58cf897dfe0c673937e52873c50f752043cd96
Daniel Arriero <DanielArr@youforce.net>
2020-04-22 23:18:13 +0200

    Added documentation to the methods

--- src/math/math.router.js
+++ src/math/math.router.js
@@ -4,7 +4,18 @@ const controller = require('./math.controller');
 const rules = require('./math.rules');
 
 const router = express.Router();
+
+/**
+ * Return the Next Number from the Integer Passed
+ * @param {number} num the base number to the next in line
+ */
 router.get('/next/:num', rules.getNextNumber(), common.checkValidation, controller.getNextNumber);
+
+/**
+ * Return the Sum of Two Numbers
+ * @param {number} num1 first summand in the operation
+ * @param {number} num2 second summand in the operation
+ */
 router.get('/sum2nums/:num1/:num2', rules.sumTwoNumbers(), common.checkValidation, controller.sumTwoNumbers);
 
 module.exports = router;
--- src/time/time.router.js
+++ src/time/time.router.js
@@ -4,6 +4,11 @@ const controller = require('./time.controller');
 const rules = require('./time.rules');
 
 const router = express.Router();
+
+/**
+ * Convert Minutes into Seconds
+ * @param {number} minutes the number of minutes to convert into seconds
+ */
 router.get('/m2s/:minutes', rules.minutesToSeconds(), common.checkValidation, controller.minutesToSeconds);
 
 module.exports = router;
