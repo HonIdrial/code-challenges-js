799b10530bf0d9923996f79d81fca05172333ddf
Daniel Arriero <DanielArr@youforce.net>
2020-04-24 01:58:02 +0200

    Spotted one more type of name that also follows the rules

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -3,17 +3,20 @@ module.exports = {
     // Types of valid names
     //  1. Initial and last name (H. Wells)
     //  2. Initial, mid name initial and last name (H. G. Wells)
-    //  3. Full first name, mid initial and last name
+    //  3. Full first name and last name (Herbert Wells)
+    //  3. Full first name, mid initial and last name (Herbert G. Wells)
     //  4. Initial and last name (H. Wells)
     const nameType1 = /^[A-Z]\.\s[A-Z][a-z]+$/;
     const nameType2 = /^[A-Z]\.\s[A-Z]\.\s[A-Z][a-z]+$/
-    const nameType3 = /^[A-Z][a-z]+\s[A-Z]\.\s[A-Z][a-z]+$/
-    const nameType4 = /^[A-Z][a-z]+\s[A-Z][a-z]+\s[A-Z][a-z]+$/
+    const nameType3 = /^[A-Z][a-z]+\s[A-Z][a-z]+$/
+    const nameType4 = /^[A-Z][a-z]+\s[A-Z]\.\s[A-Z][a-z]+$/
+    const nameType5 = /^[A-Z][a-z]+\s[A-Z][a-z]+\s[A-Z][a-z]+$/
 
     const isValidName = nameType1.test(req.params.name)
       || nameType2.test(req.params.name)
       || nameType3.test(req.params.name)
-      || nameType4.test(req.params.name);
+      || nameType4.test(req.params.name)
+      || nameType5.test(req.params.name);
 
     res.json({ isValidName: isValidName });
   }
